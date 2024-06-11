34a047be80f72f2b98b9f3d191a0a31b53a8fe9d
lee-cheolhee <cheolhee.lee@farmily.ai>
2024-06-11 13:36:49 +0900

    plc 신호에 맞춰서 capture 함수 호출되도록 수정, 캡처된 이미지와 pcd 데이터 구독/발행 함수 추가

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 source /opt/ros/noetic/setup.bash
 source /root/catkin_ws/install/setup.bash
 
-roslaunch --wait zivid_sample rdv_zivid.launch
+roslaunch --wait zivid_samples rdv_zivid.launch
 
 exec "$@"
--- zivid_samples/src/rdv_capture_with_settings_from_yml.cpp
+++ zivid_samples/src/rdv_capture_with_settings_from_yml.cpp
@@ -8,7 +8,22 @@
 #include <ros/ros.h>
 #include <ros/package.h>
 #include <std_msgs/String.h>
+
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/sync_policies/exact_time.h>
+
 #include <boost/thread.hpp>
+
+ros::Publisher img_pub;
+ros::Publisher pc_pub;
+bool pub_flag = false;
 
 #define CHECK(cmd)                                                                                                     \
   do                                                                                                                   \
@@ -34,16 +49,39 @@ namespace
 
 void plcCallback(const std_msgs::String::ConstPtr& msg)
 {
-    // TODO: plc 입력에 따라 capture 호출하도록 기능 추가
-    ROS_INFO("Calling capture");
-    capture();
+    if (msg->data == "robot_start")
+    {
+        ROS_INFO("Calling capture");
+        capture();
+        pub_flag = true;
+    }
+    else
+        pub_flag = false;
+}
+
+void dataCallback(
+    const boost::shared_ptr<const sensor_msgs::Image>& in_img_msg,
+    const boost::shared_ptr<const sensor_msgs::PointCloud2>& in_pc2_msg
+)
+{
+    ROS_INFO("Callback message filter");
+
+    if (pub_flag)
+    {
+        img_pub.publish(in_img_msg);
+        pc_pub.publish(in_pc2_msg);
+
+        ROS_INFO("Image / PCD Publish!!!");
+    }
+    else
+        ROS_INFO("Stop pub");
 }
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "rdv_capture_with_settings_from_yml_cpp");
     ros::NodeHandle nh;
-    
+
     ROS_INFO("Starting rdv_capture_with_settings_from_yml.cpp");
     
     CHECK(ros::service::waitForService("/zivid_camera/capture", default_wait_duration));
@@ -58,8 +96,18 @@ int main(int argc, char** argv)
     load_settings_from_file.request.file_path = settings_path;
     CHECK(ros::service::call("/zivid_camera/load_settings_from_file", load_settings_from_file));
     
-    ros::Subscriber plc_sub = nh.subscribe("/rdv_pcl/signal", 1, plcCallback);
-    
+    ros::Subscriber plc_sub = nh.subscribe("/rdv_plc/request", 1, plcCallback);
+
+    img_pub = nh.advertise<sensor_msgs::Image>("/zivid_camera/color/image_color", 1);
+    pc_pub = nh.advertise<sensor_msgs::PointCloud2>("/zivid_camera/points/xyzrgba", 1);
+
+    message_filters::Subscriber<sensor_msgs::Image> img_sub(nh, "/zivid_camera/color/image_color", 1);
+    message_filters::Subscriber<sensor_msgs::PointCloud2> pc_sub(nh, "/zivid_camera/points/xyzrgba", 1);
+
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::PointCloud2> MySyncPolicy;
+    message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(1), img_sub, pc_sub);
+    sync.registerCallback(boost::bind(&dataCallback, _1, _2));
+
     ros::waitForShutdown();
     
     return 0;
